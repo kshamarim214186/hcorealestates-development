adec89898db8df3140f83baf377555dd7209d481
kshamarim214186 <kshamarim@gmail.com>
2024-05-03 23:53:37 +0530

    update

--- src/app/components/ProjectByListing.jsx
+++ src/app/components/ProjectByListing.jsx
@@ -1,12 +1,8 @@
-import { useRouter } from 'next/navigation'
-import React, { useEffect, useState } from 'react';
 import ListItems from "../UI/ListItems";
 import styles from "../scss/developer.module.scss";
 import CustomPagination from "@/app/components/CustomPagination";
-import LoadingCustom from './loading-custom';
 
 export default function ProjectByListing({ resultProperties, pageName, currentpage }) { 
-   const router = useRouter()  
    const properties = resultProperties.propertydata;
    const message = resultProperties.message;
    const totalrecords = resultProperties.totalrecords;
@@ -15,21 +11,7 @@ export default function ProjectByListing({ resultProperties, pageName, currentpa
    
    return ( 
       <> 
-         <div className={styles.counts}>
-            {Number(totalrecords)} {Number(totalrecords) <= 1 ? "Property" : "Properties"} Found
-         </div>
-         {properties.map(function (property) {
-          return (
-            <ListItems itemObj={property} key={property.id} />
-          );
-         })}
-         {message == 'success' &&
-          <div className="col-12">
-            <div className={styles.paginContainer}>
-              <CustomPagination totalrecord={totalrecords} pagename={pageName} currentpage={currentpage} numberofpage={number_of_page} />
-            </div>
-          </div>
-         }
+         <p>cvxv</p>
       </>
    );
 }
--- src/app/project/page.jsx
+++ src/app/project/page.jsx
@@ -1,5 +1,4 @@
 "use client";
-import { Suspense } from 'react'
 import { useSearchParams } from 'next/navigation'
 import styles from "../scss/properties.module.scss";
 import Breadcrumb from "react-bootstrap/Breadcrumb";
@@ -71,11 +70,9 @@ export default async function Project() {
                         <SortFilter sortObj={sort} currentpage={currentpage} />
                       </div>
                     </div>
-                    <Suspense fallback={<LoadingCustom />}>
-                       <div className={styles.allList}>
-                           <ProjectByListing resultProperties={resultProperties} pageName={pageName} currentpage={currentpage} />
-                       </div>
-                    </Suspense> 
+                    <div className={styles.allList}>
+                        <ProjectByListing resultProperties={resultProperties} pageName={pageName} currentpage={currentpage} />
+                    </div>
                   </div>
                </div>               
             </div>
