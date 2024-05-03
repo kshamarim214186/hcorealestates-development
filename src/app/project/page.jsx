cb6538b04a0e8eedc15276ca819914773a94890f
kshamarim214186 <kshamarim@gmail.com>
2024-05-04 00:42:51 +0530

    update

--- src/app/project/page.jsx
+++ src/app/project/page.jsx
@@ -46,29 +46,7 @@ export default async function Project() {
       <>
       <Header resultHeader={result} commercialData={commercialData} residentialData={residentialData} />   
          <main className={`${styles.container} container-xl`}>
-            <title>{result.pagedata.proplistseotitle}</title>
-            <meta name="description" content={result.pagedata.proplistseodesc} />
-            <link rel="canonical" href={canonicalUrl} />
-            <Breadcrumb className={styles.bredcurmb}>
-                <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
-                <Breadcrumb.Item active>Project</Breadcrumb.Item>
-            </Breadcrumb>
-
-           
-            <div className="row">
-               <div className="col-lg-12">
-                  <div className={styles.overview}>
-                     <h1>{result.pagedata.proplisth1}</h1>
-                     <p>{result.pagedata.proplistshortdesc}</p>
-                     <Accordion defaultActiveKey="0">
-                        <Accordion.Item eventKey="1">
-                           <Accordion.Body>{result.pagedata.proplistfulldesc}</Accordion.Body>
-                           <Accordion.Header as={"div"}></Accordion.Header>
-                        </Accordion.Item>
-                     </Accordion>
-                  </div>
-               </div>
-            </div> 
+            
          </main>
       <Footer resultFooter={result} commercialData={commercialData} residentialData={residentialData} pageName={pageName} projectName={projectName} />
       </>
