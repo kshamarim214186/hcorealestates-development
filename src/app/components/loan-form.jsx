708bc57b5059c13b142927a8cc456a16ce1a601a
kshamarim214186 <kshamarim@gmail.com>
2026-02-23 15:56:06 +0530

    loan form

--- src/app/components/loan-form.jsx
+++ src/app/components/loan-form.jsx
@@ -1,10 +1,43 @@
 'use client';
+import { useRouter } from 'next/navigation'
 import { useState } from 'react';
 export default function LoanForm() {
+
+  const  projectName = 'Loan Form';
+  const router = useRouter()
+   const [isSubmitting, setIsSubmitting] = useState(false);
+   const [error, setError] = useState('');
   const [name, setName] = useState('');
-  const [phone, setPhone] = useState('');
-  const formSubmit = e => {
+  const [email, setEmail] = useState("noreply@gmail.com");
+   const [country, setCountry] = useState("2");
+  const [mobileNo, setPhone] = useState('');
+  const formSubmit = async (e) => {
     e.preventDefault();
+    setIsSubmitting(true);     
+      const formData = new URLSearchParams();
+      formData.append('token1', process.env.token1);
+      formData.append('token2', process.env.token2);
+      formData.append('name', name);
+      formData.append('email', email);
+      formData.append('country', country);
+      formData.append('mobileNo', mobileNo);
+      formData.append('projectName', projectName);
+      const finalresult = await fetch(process.env.API_URL+'users/submitQueryForm/', {
+         method: 'POST',
+         headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+         },
+         body: formData,
+      }); 
+      const data = await finalresult.json();  
+      
+      if(data.error == true){
+         setIsSubmitting(false);
+         setError(data.message);
+      }else{
+         setIsSubmitting(true);
+         router.push('thank-you');
+      }
   };
   return (
     <>
@@ -21,12 +54,10 @@ export default function LoanForm() {
             type="tel"
             className="form-control"
             placeholder="Enter Phone Number"
-            value={phone}
+            value={mobileNo}
             onChange={e => setPhone(e.target.value)}
           />
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
+          <button disabled={isSubmitting} type="submit" className="btn btn-primary">{isSubmitting ? "Submitting..." : "Submit"}</button>
         </div>
       </form>
     </>
